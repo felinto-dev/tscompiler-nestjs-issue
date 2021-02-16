44da1063ac88bcae65565f69a78b6063a8c28e21
Felinto <emersong20.email@gmail.com>
2021-02-16 13:52:26 -0300

    reproduce bug

--- src/recipes/models/recipe.model.ts
+++ src/recipes/models/recipe.model.ts
@@ -5,15 +5,11 @@ export class Recipe {
   @Field(type => ID)
   id: string;
 
-  @Field()
   title: string;
 
-  @Field({ nullable: true })
   description?: string;
 
-  @Field()
   creationDate: Date;
 
-  @Field(type => [String])
   ingredients: string[];
 }
